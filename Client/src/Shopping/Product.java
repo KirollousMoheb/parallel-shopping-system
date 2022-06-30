24c6a59936e4febab770784a65343b9281319a06
engy2000 <85259779+engy2000@users.noreply.github.com>
2022-06-30 13:24:12 +0200

    Add files via upload

---
+++ Client/src/Shopping/Product.java
@@ -0,0 +1,28 @@
+package Shopping;
+
+public class Product {
+    String quantity;
+    String name;
+
+    public Product(String name, String quantity) {
+        this.name = name;
+        this.quantity = quantity;
+
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
